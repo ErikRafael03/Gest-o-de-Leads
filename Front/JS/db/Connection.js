5caf27988b437edee942737a35c4429aeedb4866
ItaloRuann21 <italotavares751@gmail.com>
2023-11-20 21:11:59 -0300

    dados inseridos no mongodb

--- Front/JS/db/Connection.js
+++ Front/JS/db/Connection.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose")
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
 
-const connect = () => {
-    mongoose.connect('mongodb+srv://Erik:erik01042003@erik.ett1yrz.mongodb.net/?retryWrites=true&w=majority', {
-        useNewUrlParser: false,
-        useUnifiedTopology: false,
-    })
-    const connection = mongoose.connection;
+// Estabelecer conexão com o banco de dados
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+const connection = mongoose.connection
 
-    connection.on("error", () => {
-        console.error("Erro ao conectar com o banco")
-    })
+connection.on("error", console.error.bind(console, "connection error:"))
+connection.once("open", () => {
+  console.log("Conexão bem-sucedida com o banco de dados!")
+})
 
-    connection.on("open", () => {
-        console.log("Conectado com sucesso!")
-    })
-}
+connect()
 
-connect();
-
-module.exports = mongoose;
+module.exports = mongoose
--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,80 +1,79 @@
-const readExcel = require('read-excel-file/node');//usado para trabalhar com qualquer arquivo excel
-const express = require('express')// usado para trabalhar com api
+const readExcel = require("read-excel-file/node")
+const express = require("express")
 const app = express()
-const cors = require('cors');
-app.use(cors());
-const path = require('path')// usado para interagir com arquivos
-const fs = require('fs')// usado para interagir com arquivos
+const cors = require("cors")
+const path = require("path")
+const fs = require("fs")
 const port = 2000
-const db = require('./db/Connection')//Conexão com o banco
-const model = require('./db/models/LeadModel');
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI);
-const { error } = require('console');
-app.use(express.json()); //enviar dados no corpo da requisição
+const mongoose = require("mongoose")
+const model = require("./db/models/LeadModel")
+require("dotenv").config()
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+const db = mongoose.connection
+
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+  console.log("Conexão bem-sucedida com o banco de dados!")
+})
+
+app.use(cors())
+app.use(express.json())
 
 async function getContent(files) {
-    const result = []
-    for await (const file of files) {
-        await readExcel(fs.createReadStream(file)).then((table) => {
-            result.push(table)
-        })
-    }
-    return result
+  const result = []
+  for await (const file of files) {
+    await readExcel(fs.createReadStream(file)).then((table) => {
+      result.push(table)
+    })
+  }
+  return result
 }
 
-app.post('/',/* upload.single("arquivo"),*/ async (req, res) => {
-    const files = [path.join(__dirname, /*getFile()*/'./arquivo.xlsx')]
-    const result = await getContent(files);
-    var lista_dados = [];
-    for (var i = 1; i < result[0].length; i++) {
-        var registro = {
-            "Inscricao": result[0][i][0],
-            "Dt_Inscricao": result[0][i][1],
-            "Nome": result[0][i][2],
-            "Email": result[0][i][3],
-            "Num_Telefone": result[0][i][4],
-            "Classificacao": result[0][i][5],
-            "Dt_Classificacao": result[0][i][6],
-            "Curso": result[0][i][7],
-            "Duracao": result[0][i][8],
-            "CODCurso": result[0][i][9],
-            "Turno": result[0][i][10],
-            "Filial": result[0][i][11],
-            "Modalidade": result[0][i][12],
-            "Polo": result[0][i][13],
-            "Semestre": result[0][i][14],
-            "Valor_Matricula": result[0][i][15],
-            "ContratoAceito": result[0][i][16],
-            "Tipo_de_Aceite": result[0][i][17],
-            "IP_do_Aceite": result[0][i][18]
-        };
-        lista_dados[i - 1] = registro;
-        db.connect(model).then((erro, banco) => {
-            if (erro) throw erro
-            var dbo = banco.db("test")
-            var obj = { registro }
-            var Colecao = "leads"
-            console.log(dbo);
-        })
-            .catch((erro, obj, banco, Colecao, dbo) => {
-                dbo.collection(Colecao).insertOne(obj, (erro, result) => {
-                    if (erro) throw erro
-                    console.log("Inserido com sucesso!!")
-
-                    banco.close()
-                })
-
-            })
-
+app.post("/", async (req, res) => {
+  const files = [path.join(__dirname, "./arquivo.xlsx")]
+  const result = await getContent(files)
+  const lista_dados = []
+  for (let i = 1; i < result[0].length; i++) {
+    const registro = {
+      Inscricao: result[0][i][0],
+      Dt_Inscricao: result[0][i][1],
+      Nome: result[0][i][2],
+      Email: result[0][i][3],
+      Num_Telefone: result[0][i][4],
+      Classificacao: result[0][i][5],
+      Dt_Classificacao: result[0][i][6],
+      Curso: result[0][i][7],
+      Duracao: result[0][i][8],
+      CODCurso: result[0][i][9],
+      Turno: result[0][i][10],
+      Filial: result[0][i][11],
+      Modalidade: result[0][i][12],
+      Polo: result[0][i][13],
+      Semestre: result[0][i][14],
+      Valor_Matricula: result[0][i][15],
+      ContratoAceito: result[0][i][16],
+      Tipo_de_Aceite: result[0][i][17],
+      IP_do_Aceite: result[0][i][18],
     }
+    lista_dados.push(registro)
+  }
 
-    res.json(registro);
-});
+  // Inserir os registros no banco de dados
+  const collection = db.collection("leads")
+  collection.insertMany(lista_dados, (err, result) => {
+    if (err) {
+      console.error("Erro ao inserir registros:", err)
+      res.status(500).json({ error: "Erro ao inserir registros" })
+    } else {
+      console.log("Registros inseridos com sucesso!!")
+      res.json({ message: "Registros inseridos com sucesso" })
+    }
+  })
+})
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
-
-require("./db/Connection")
+  console.log(`App listening on port ${port}`)
+})
